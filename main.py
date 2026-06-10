36cbc44d1cc993d69fa0467c9314635d3ca25a59
lusanajoachim7-svg <lusanajoachim7@gmail.com>
2026-06-10 13:16:31 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class LecsLusanaApp(App):
+    def build(self):
+        return Label(text='LECS LUSANA INAFANYA KAZI!\nTUMESHINDA COLAB', font_size='20sp')
+
+if __name__ == '__main__':
+    LecsLusanaApp().run()
